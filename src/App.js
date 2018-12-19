0edf2e9ceb907ef3f3fe00ff16e3ba1da2c9c933
Cian Collier <ciancollier17@gmail.com>
2018-12-19 22:08:39 +0000

    Changed algorithm to take account of order of operations. Still an error in this.

--- src/App.js
+++ src/App.js
@@ -9,40 +9,72 @@ class App extends Component {
     buttons: [1, 2, 3, "+", 4, 5, 6, "-", 7, 8, 9, "X", 0, ".", "C", "/", "="]
   }
 
+  performOperation = (operands) => {
+    switch (operands[1]) {
+      case '+':
+        return operands[0] + operands[2];
+        break;
+      case '-':
+        return operands[0] - operands[2];
+        break;
+      case 'X':
+        return operands[0] * operands[2];
+        break;
+      case '/':
+        return operands[0] / operands[2];
+        break;
+    }
+  }
+
   onButtonClick = (button_value) => {
     let new_screen_content = "";
 
     if (button_value == "=") {
       let answer = 0;
       let input = this.state.screen_content.split('');
-      let mode = '+';
       let holder = "";
+      let operationsCounter = 0;
+      let operations = [];
 
       for (let i = 0; i < input.length; i++) {
-        if (input[i] == "+" || input[i] == "-" || input[i] == "/" || input[i] == "X" || i == (input.length - 1)) {
+        if (input[i] == '+' || input[i] == '-' || input[i] == 'X' || input[i] == '/' || i == (input.length - 1)) {
           if (i == (input.length - 1)) {
             holder += input[i];
           }
 
-          switch (mode) {
-            case "+":
-              answer += parseFloat(holder);
-              break;
-            case "-":
-              answer -= parseFloat(holder);
-              break;
-            case "X":
-              answer *= parseFloat(holder);
-              break;
-            case "/":
-              answer /= parseFloat(holder);
-              break;
+          if (operations.length !== 0) {
+            operations[operationsCounter].push(parseFloat(holder));
+            operationsCounter++;
           }
+
+          if (i !== (input.length - 1)) {
+            operations.push([parseFloat(holder), input[i]]);
+          }
+
           holder = "";
-          mode = input[i];
-        }
-        else {
+        } else {
           holder += input[i];
+        }
+      }
+
+      let bodmas = ['/', 'X', '+', '-'];
+
+      for (let c = 0; c < bodmas.length; c++) {
+        for (let d = 0; d < operations.length; d++) {
+          if (operations[d][1] == bodmas[c]) {
+            let result = this.performOperation(operations[d]);
+            answer = result;
+            if (d !== 0) {
+              operations[d - 1][2] = result;
+            }
+
+            if (d !== (operations.length - 1)) {
+              operations[d + 1][0] = result;
+            }
+
+            operations.splice(d, 1);
+            d = -1;
+          }
         }
       }
 
